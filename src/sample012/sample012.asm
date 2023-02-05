d84a42404e4d090cfaaa57c542b20c4888f99e74
brapunch2000 <brapunch2000@brapunch2000noMacBook-Pro.local>
2023-02-05 19:05:05 +0900

    sample012 added.

--- src/sample012/sample012.asm
+++ src/sample012/sample012.asm
@@ -100,15 +100,14 @@ EnemyPtrTblInitLoop:
 ;-------------------------------------------------
 ; メインループ(割り込み処理でGameProcを呼び出す)
 ;-------------------------------------------------
-    ;ld a, 0
-    ;ld (VSYNC_WAIT_CNT), a
+    ld a, 0
+    ld (VSYNC_WAIT_CNT), a
 
-    ;call INIT_H_TIMI_HANDLER
+    call INIT_H_TIMI_HANDLER
 
 ;--------------------------------------------
 ; BGM演奏開始
 ;--------------------------------------------
-    call SOUNDDRV_INIT
     ld hl, BGM_00
     call SOUNDDRV_BGMPLAY
 
@@ -118,18 +117,17 @@ MainLoop:
     ;   - メインロジック開始時に 0 に設定
     ;   - H.TIMI割り込み処理の中でデクリメント (1/60秒ごとに呼び出し)
     ;   - メインロジックの最後に、キャリーがONになるまで待機
-    ; ld a, 1
-    ; ld (VSYNC_WAIT_CNT), a
+    ld a, 1
+    ld (VSYNC_WAIT_CNT), a
 
     ; ゲーム処理呼び出し
-
     call GameProc
 
 VSYNC_Wait:
     ; 垂直帰線待ち
-    ; ld a, (VSYNC_WAIT_CNT)
-    ; or a
-    ; jr nz,VSYNC_Wait
+    ld a, (VSYNC_WAIT_CNT)
+    or a
+    jr nz,VSYNC_Wait
 
     jr MainLoop
 
@@ -204,7 +202,7 @@ GameProc_PlayerMove:
 
     call MovePlayer
 
-    call DebugPrint
+    ; call DebugPrint
 
 GameProcEnd:
 
