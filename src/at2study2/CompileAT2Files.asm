de7e1a620580672ac851ce86f9a927f93c2a73a4
brapunch2000 <brapunch2000@brapunch2000noMacBook-Pro.local>
2023-11-19 15:19:59 +0900

    at2study2 add

---
+++ src/at2study2/CompileAT2Files.asm
@@ -0,0 +1,21 @@
+;Compiles the player, the music and sfxs, using RASM.
+;No ORG needed.
+
+    ;This is the music, and its config file.
+    include "TRAINING_BGM.asm" 
+    include "TRAINING_BGM_playerconfig.asm" 
+    include "TRAINING_SFX.asm" 
+    include "TRAINING_SFX_playerconfig.asm" 
+ 
+    ;What hardware? Uncomment the right one.
+    PLY_AKG_HARDWARE_MSX = 1        
+
+    ;Comment/delete this line if not using sound effects.
+    PLY_AKG_MANAGE_SOUND_EFFECTS = 1
+
+    ;ROM
+    PLY_AKG_Rom = 1
+    PLY_AKG_ROM_Buffer = #d900
+
+    ;This is the player.
+    include "PlayerAkg.asm"
